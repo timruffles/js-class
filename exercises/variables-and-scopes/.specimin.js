ea0083771c24575d32371f9abbc66c1cf7952116
Tim Ruffles <timruffles@gmail.com>
2016-01-24 17:38:01 +0000

    variable test

--- exercises/variables-and-scopes/.specimin.js
+++ exercises/variables-and-scopes/.specimin.js
@@ -1,1 +1,15 @@
 
+Object.assign(exports, require("./exercise"));
+
+// ignore this :)
+exports.ANSWERS = {
+  scenarioOne: "inner",
+  scenarioTwo: "out",
+  scenarioThree: "helper",
+  scenarioFour: "outer",
+  scenarioFive: "inner",
+  scenarioSix: 10,
+  scenarioSeven: 6,
+}
+
+
--- exercises/variables-and-scopes/.test.js
+++ exercises/variables-and-scopes/.test.js
@@ -1,11 +1,34 @@
 var help = require("../.sys/test-help");
 
-describe("", function() {
+describe("variables and scopes", function() {
 
     help.importAndTest(__dirname, function(exported) {
 
-      it("", function() {
-      });
+      for(let scenario in exported) {
+        if(!/^scenario/.test(scenario)) {
+          continue;
+        }
+
+        let answered = exported.ANSWERS[scenario];
+
+        it(`${scenario} - correct answer`, function() {
+          if(answered === "TODO") {
+            throw new Error("unanswered");
+            return;
+          }
+
+          try {
+            var actual = exported[scenario]();
+          } catch(e) {
+            assert(answered == undefined, "should have been null");
+            assert(answered == undefined, "incorrect");
+          }
+          assert.equal(answered, actual, "incorrect");
+          assert(answered === actual, "incorrect");
+        });
+      }
+
+
 
     });
 
