dfc5b938348cf835fc69afcc3a22c37c90767b71
Tim Ruffles <timruffles@gmail.com>
2016-01-25 01:35:45 +0000

    test

--- exercises/maps-objects-sets/.test.js
+++ exercises/maps-objects-sets/.test.js
@@ -1,11 +1,70 @@
 var help = require("../.sys/test-help");
+var _ = require("lodash");
 
-describe("", function() {
+describe("maps objects sets", function() {
 
     help.importAndTest(__dirname, function(exported) {
 
-      it("", function() {
-      });
+      describe('getCount', function() {
+        it("counts events", function() {
+
+          const [a,b,c] = _.sampleSize(["banana","monkey","cat","mat","spoon","hat"], 3)
+
+          exported.getCount(function(fn) {
+
+            fn(a)
+            fn(b)
+            fn(b)
+            fn(a)
+            fn(b)
+            fn(a)
+            assert.equal(fn(a), 4);
+            assert.equal(fn(c), 1);
+            assert.equal(fn(b), 4);
+            
+          });
+
+        });
+          
+      })
+
+      describe('getIpSets', function() {
+        it("counts events", function() {
+
+          const [a,b,c] = _.shuffle(["clap","snap"]);
+          const [ipA, ipB, ipC] = [randIp(), randIp(), randIp()];
+
+          exported.getIpSets(function(fn) {
+
+            fn(a, ipA)
+            fn(a, ipA)
+            fn(a, ipC)
+            assert.sameMembers(entries(fn(a, ipA)), [ipA,ipC]);
+            
+            fn(b, ipB)
+            assert.sameMembers(entries(fn(b, ipB)), [ipB]);
+          });
+
+          function randIp() {
+
+            return [block(),block(),block(),block()].join(".")
+            
+            function block() {
+              return Math.round(Math.random() * 255);
+            }
+          }
+
+          function entries(x) {
+            const es = [];
+            for(let v of x) {
+              es.push(v);
+            }
+            return es;
+          }
+
+        });
+          
+      })
 
     });
 
--- exercises/maps-objects-sets/exercise.js
+++ exercises/maps-objects-sets/exercise.js
@@ -1,1 +1,37 @@
+// TODO function passed to incoming will be called
+// a number of times with different event strings
+//
+// return the number of times an event has been
+// called on each event
+//
+//
+// try ES5 and ES6 implementations
+//
+exports.getCount = function(incoming) {
+
+  incoming(function(event) {
+
+    // TODO
+    return 0;
+  })
+  
+};
+
+// TODO function passed to incoming will be called
+// a number of times with different event/ip combinations
+//
+// return the unique set of IPs for each event on each event,
+// as maps or arrays
+//
+// try ES5 and ES6 implementations
+//
+exports.getIpSets = function(incoming) {
+
+
+  incoming(function(event, ip) {
+
+    // TODO
+    return [];
+  })
+};
 
