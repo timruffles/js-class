3b545255dfbff42459cfe570f61dada71c02f74e
Tim Ruffles <timruffles@gmail.com>
2016-04-19 23:04:37 +0100

    [fix] lint

--- exercises/destructuring/exercise.js
+++ exercises/destructuring/exercise.js
@@ -1,8 +1,8 @@
 
 // part 1: getting rid of temporary vars
 
-let up = "Up!";
-let down = "Down!";
+let up = 'Up!';
+let down = 'Down!';
 
 // TODO make Alice drink a potion - switch up and down in one line!
 // this is tricky - perhaps you could destructure an array some how?
@@ -12,7 +12,7 @@ export { up, down };
 
 // part 2: arrays
 //
-const raceParticipants = ["Hare", "Alice", "Cheshire Cat"];
+const raceParticipants = ['Hare', 'Alice', 'Cheshire Cat'];
 
 let winner;
 let others;
@@ -26,7 +26,7 @@ export { winner as raceWinner, others as raceOthers };
 
 // part 3: objects
 
-const guest = { name: "Hare", treat: "chocolate eggs" };
+const guest = { name: 'Hare', treat: 'chocolate eggs' };
 
 let guestName;
 
