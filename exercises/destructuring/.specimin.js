6002b13293060e0e30898f98e94d8e22975aea3d
Edvin Salo <best.fast@Edvins-Mac.local>
2016-04-19 05:10:00 -0700

    working on destructuring, destructuring.md file pre-completed

---
+++ exercises/destructuring/.specimin.js
@@ -0,0 +1,15 @@
+
+Object.assign(exports, require("./exercise"));
+
+// ignore this :)
+export const ANSWERS = {
+  scenarioOne: "inner",
+  scenarioTwo: "out",
+  scenarioThree: "helper",
+  scenarioFour: "outer",
+  scenarioFive: "inner",
+  scenarioSix: 10,
+  scenarioSeven: 6,
+}
+
+
