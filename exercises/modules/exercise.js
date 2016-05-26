0e886b94ed66586e40f4643919bde1ce7d1e4412
Tim Ruffles <timruffles@gmail.com>
2016-05-26 09:26:17 +0100

    [fix] completed

--- exercises/modules/exercise.js
+++ exercises/modules/exercise.js
@@ -23,37 +23,3 @@ var hattersTreat = 'darjeeling in a conch-shell';
 
 // TODO export the Hatter as default!
 // TODO export the hattersTreat as treat
-export {
-  Hatter as default,
-  hattersTreat as treat
-};
-
-
-
-/**
- * 2) learning to import with the March Hare
- *
- * module is './MarchHare'
- */
-
-
-import hareDefault from './MarchHare';
-import * as wholeMarchHareModule from './MarchHare';
-import { treat as MarchHareTreat } from './MarchHare';
-
-// TODO import the March Hare's default, and its treat
-
-// TODO rexport as MarchHare and MarchHareTreat
-export { hareDefault as MarchHare, MarchHareTreat };
-
-
-
-/**
- * 2) learning to import & export in one go with Alice
- *
- * module is './Alice'
- */
-
-// TODO using the rexport syntax, import and rexport Alice (Alice module default) and AliceTreat
-
-export { default as Alice, treat as AliceTreat } from './Alice';
