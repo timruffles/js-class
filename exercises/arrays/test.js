6ece3dd66977032f1ccd9587155a15eec7635e4b
Tim Ruffles <timruffles@gmail.com>
2018-03-13 08:56:30 +0000

    makes more sense to KISS - allows for better linting/formatting

--- exercises/arrays/test.js
+++ exercises/arrays/test.js
@@ -3,29 +3,32 @@ import * as specimin from "./.specimin.js";
 import * as yourSolution from "./exercise.js";
 
 const solution = window.location.search.includes('specimin') ? specimin : yourSolution;
-test("can recurse through objects", function() {
 
-            const [a,b,c] = [help.rstring(), help.rstring(), help.rstring()];
-            const object =  {[a]: {[b]: {[c]: "got it" } } };
-        
+let a,b,c;
+let object;
+let withNull;
 
-            assert.equal(solution.dig(object, a,b,c), "got it");
-         })
+beforeEach(function() {
+    [a,b,c] = [help.rstring(), help.rstring(), help.rstring()];
+    object =  {[a]: {[b]: {[c]: "got it" } } };
+    withNull =  {[a]: null };
+});
 
-test("works with no properties", function() {
+test("can look up multiple properties deep in objects", function() {
 
-            const [a,b,c] = [help.rstring(), help.rstring(), help.rstring()];
-            const object =  {[a]: {[b]: {[c]: "got it" } } };
-        
-
-            assert.equal(solution.dig(object), undefined);
+            assert.equal(solution.dig(object, [a,b,c]), "got it");
          })
 
 test("safely returns null-ish value when can't dig further", function() {
 
-            const [a,b,c] = [help.rstring(), help.rstring(), help.rstring()];
-            const object =  {[a]: {[b]: {[c]: "got it" } } };
-        
+            assert.equal(solution.dig(object, ['z','z','z']), undefined);
+         })
 
-            assert.equal(solution.dig(object, 'z','z','z'), undefined);
+test("works with no properties provided", function() {
+
+            assert.equal(solution.dig(object, []), undefined);
+         })
+
+test("returns null if hits a null", function() {
+            assert.equal(solution.dig(withNull, ['z']), null);
          })
